b5fb1f7966396984204ac291cc2e358baec972a8
Studente 4^D <studente4i@lab-inf1-pc28.itimarzotto.local>
2022-05-30 09:27:44 +0200

    si

---
+++ Ball.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBall
+{
+    class Ball
+    {
+        Form1 f;
+        private int ballWidth = 300;
+        private int ballHeight = 300;
+        private int ballPosX = 0;
+        private int ballPosY = 0;
+        private int moveStepX = 4;
+        private int moveStepY = 4;
+
+        public Ball(Form1 f1)
+        {
+            f = f1;
+        }
+
+        public void Paint(PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode =
+                System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            e.Graphics.Clear(f.BackColor);
+
+            e.Graphics.FillEllipse(Brushes.Red,
+                ballPosX, ballPosY,
+                ballWidth, ballHeight);
+
+            e.Graphics.DrawEllipse(Pens.Black,
+                ballPosX, ballPosY,
+                ballWidth, ballHeight);
+        }
+
+        public void Move()
+        {
+            ballPosX += moveStepX;
+            if (ballPosX < 0 || ballPosX + ballWidth > f.ClientSize.Width)
+            {
+                moveStepX = -moveStepX;
+            }
+
+            ballPosY += moveStepY;
+            if (ballPosY < 0 || ballPosY + ballHeight > f.ClientSize.Height)
+            {
+                moveStepY = -moveStepY;
+            }
+        }
+    }
+}
--- Form1.cs
+++ Form1.cs
@@ -12,16 +12,11 @@ namespace BouncingBall
 {
     public partial class Form1 : Form
     {
-        private int ballWidth = 300;
-        private int ballHeight = 300;
-        private int ballPosX = 0;
-        private int ballPosY = 0;
-        private int moveStepX = 4;
-        private int moveStepY = 4;
-
+        Ball b;
         public Form1()
         {
             InitializeComponent();
+            b = new Ball(this);
 
             this.SetStyle(
                 ControlStyles.OptimizedDoubleBuffer |
@@ -35,42 +30,12 @@ namespace BouncingBall
 
         private void PaintCircle(object sender, PaintEventArgs e)
         {
-            e.Graphics.SmoothingMode = 
-                System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-
-            e.Graphics.Clear(this.BackColor);
-
-            e.Graphics.FillEllipse(Brushes.Red,
-                ballPosX, ballPosY,
-                ballWidth, ballHeight);
-
-            e.Graphics.DrawEllipse(Pens.Black,
-                ballPosX, ballPosY,
-                ballWidth, ballHeight);
+            b.Paint(e);
         }
 
         private void MoveBall(object sender, EventArgs e)
         {
-            // update coordinates
-            ballPosX += moveStepX;
-            if (
-                ballPosX < 0 ||
-                ballPosX + ballWidth > this.ClientSize.Width
-                )
-            {
-                moveStepX = -moveStepX;
-            }
-
-            ballPosY += moveStepY;
-            if (
-                ballPosY < 0 ||
-                ballPosY + ballHeight > this.ClientSize.Height
-                )
-            {
-                moveStepY = -moveStepY;
-            }
-
-            // update painting
+            b.Move();
             this.Refresh();
         }
     }
