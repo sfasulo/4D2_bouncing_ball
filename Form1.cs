80d01c99ed6574de8d71322c9e032951f9501ac4
Docente Informatica <docentei@lab-inf1-ins1.itimarzotto.local>
2022-05-30 08:23:58 +0200

    Init commit

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBall
+{
+    public partial class Form1 : Form
+    {
+        private int ballWidth = 300;
+        private int ballHeight = 300;
+        private int ballPosX = 0;
+        private int ballPosY = 0;
+        private int moveStepX = 4;
+        private int moveStepY = 4;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.SetStyle(
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint |
+                ControlStyles.UserPaint,
+                true
+                );
+
+            this.UpdateStyles();
+        }
+
+        private void PaintCircle(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = 
+                System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            e.Graphics.Clear(this.BackColor);
+
+            e.Graphics.FillEllipse(Brushes.Red,
+                ballPosX, ballPosY,
+                ballWidth, ballHeight);
+
+            e.Graphics.DrawEllipse(Pens.Black,
+                ballPosX, ballPosY,
+                ballWidth, ballHeight);
+        }
+
+        private void MoveBall(object sender, EventArgs e)
+        {
+            // update coordinates
+            ballPosX += moveStepX;
+            if (
+                ballPosX < 0 ||
+                ballPosX + ballWidth > this.ClientSize.Width
+                )
+            {
+                moveStepX = -moveStepX;
+            }
+
+            ballPosY += moveStepY;
+            if (
+                ballPosY < 0 ||
+                ballPosY + ballHeight > this.ClientSize.Height
+                )
+            {
+                moveStepY = -moveStepY;
+            }
+
+            // update painting
+            this.Refresh();
+        }
+    }
+}
